d0f8c03793b57ed8a7eb87d92a2ecac81d155bf2
Guilherme Cavichioli <cavichioli.guilherme@gmail.com>
2024-12-05 10:41:22 -0300

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/components/**/*.story.tsx"],
+  addons: [
+    "@storybook/addon-essentials",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  webpackFinal: (config) => {
+    config.resolve?.modules?.push(`${process.cwd()}/src`);
+
+    return config;
+  },
+};
+export default config;
