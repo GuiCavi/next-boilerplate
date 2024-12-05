bba559130e5a6bab61d169d4b04ae1ba9bd0325a
Guilherme Cavichioli <cavichioli.guilherme@gmail.com>
2024-12-05 10:32:19 -0300

    update lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	// Lint & Prettify TS and JS files
-	'*.{js,jsx,ts,tsx}': (filenames) => [
-		`pnpm lint --fix . ${filenames.join(' ')}`,
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `pnpm lint --fix . ${filenames.join(' ')}`,
     `pnpm test -- --findRelatedTests ${filenames.join(' ')}`
-	]
+  ]
 }
