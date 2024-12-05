80b9f1b66f3f4e16bb9a77151e4b013a0759b614
Guilherme Cavichioli <cavichioli.guilherme@gmail.com>
2024-12-05 20:28:11 -0300

    add new dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,8 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/components/**/*.story.tsx"],
-  addons: [
-    "@storybook/addon-essentials",
-  ],
+  stories: ["../src/components/**/*.stories.tsx"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-themes"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
@@ -14,6 +12,18 @@ const config: StorybookConfig = {
     config.resolve?.modules?.push(`${process.cwd()}/src`);
 
     return config;
+  },
+  typescript: {
+    reactDocgenTypescriptOptions: {
+      // Speeds up Storybook build time
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+      // Only compile files that have stories for faster local development performance
+      include: ["src/**/*.{ts,tsx}"],
+    },
+    reactDocgen: "react-docgen",
   },
 };
 export default config;
